c928a8e50fcf2794dd02fed56d946473d2c74796
Matthew Germanowski <11788121+mjgerm@users.noreply.github.com>
2021-12-15 00:31:13 -0500

    Create ubuntu-kitchen-sink.sh

---
+++ ubuntu-kitchen-sink.sh
@@ -0,0 +1,32 @@
+# Updates
+apt-get update
+apt-get dist-upgrade
+
+# CLI tools
+snap install lxd
+apt-get install vim tmux zsh nmap
+apt-get install htop iotop iftop
+apt-get install curl wget
+
+# System tools
+apt-get install zfsutils-linux sshfs 
+apt-get install nwipe
+
+# GUI tools
+apt-get install remmina gimp vlc gnome-tweak-tool
+apt-get install virtualbox keepassxc
+
+# Development (Generic)
+snap install --classic code
+apt-get install build-essentials
+
+# Development (C)
+apt-get install cmake nasm
+
+# Development (Matlab)
+apt-get install octave octave-*
+
+# Development (Python)
+apt-get install python3 python3-pip
+pip3 install scipy matplotlib
+pip3 install requests
