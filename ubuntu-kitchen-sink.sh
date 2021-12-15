ebede0e6d8153c9a115a518aab28a7e288611a63
Matthew Germanowski <11788121+mjgerm@users.noreply.github.com>
2021-12-15 01:00:38 -0500

    Update ubuntu-kitchen-sink.sh

--- ubuntu-kitchen-sink.sh
+++ ubuntu-kitchen-sink.sh
@@ -9,13 +9,10 @@ apt-get install htop iotop iftop
 apt-get install curl wget
 
 # System tools
-apt-get install zfsutils-linux sshfs cifs-utils
-apt-get install nwipe
+apt-get install zfsutils-linux sshfs cifs-utils nwipe virtualbox
 
 # GUI tools
-apt-get install remmina gimp vlc gnome-tweak-tool
-apt-get install virtualbox keepassxc
-apt-get install gnuplot
+apt-get install remmina gimp vlc gnome-tweak-tool keepassxc gnuplot
 
 # Development (Generic)
 snap install --classic code
@@ -23,13 +20,22 @@ apt-get install build-essentials
 
 # Development (C)
 apt-get install cmake nasm gdb
+apt-get install libc-devtools libfftw3-dev
 
 # Development (Matlab)
 apt-get install octave octave-*
 
 # Development (Python)
 apt-get install python3 python3-pip
-pip3 install scipy matplotlib
-pip3 install requests beautifulsoup4
+pip3 install scipy matplotlib tensorflow
+pip3 install requests beautifulsoup4 Pillow
 pip3 install PyGTK wxPython serial
 pip3 install glances zfec
+
+# Optional Addins
+# apt-get install nvidia nvidia-cuda-toolkit
+# pip3 install cupy
+# apt-get install virtualbox-ext-pack
+
+
+
