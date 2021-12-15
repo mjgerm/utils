7deaae39f087a9bda6640854c578877ed924e61a
Matthew Germanowski <11788121+mjgerm@users.noreply.github.com>
2021-12-15 00:45:23 -0500

    Update ubuntu-kitchen-sink.sh

--- ubuntu-kitchen-sink.sh
+++ ubuntu-kitchen-sink.sh
@@ -9,19 +9,20 @@ apt-get install htop iotop iftop
 apt-get install curl wget
 
 # System tools
-apt-get install zfsutils-linux sshfs 
+apt-get install zfsutils-linux sshfs cifs-utils
 apt-get install nwipe
 
 # GUI tools
 apt-get install remmina gimp vlc gnome-tweak-tool
 apt-get install virtualbox keepassxc
+apt-get install gnuplot
 
 # Development (Generic)
 snap install --classic code
 apt-get install build-essentials
 
 # Development (C)
-apt-get install cmake nasm
+apt-get install cmake nasm gdb
 
 # Development (Matlab)
 apt-get install octave octave-*
@@ -29,4 +30,6 @@ apt-get install octave octave-*
 # Development (Python)
 apt-get install python3 python3-pip
 pip3 install scipy matplotlib
-pip3 install requests
+pip3 install requests beautifulsoup4
+pip3 install PyGTK wxPython serial
+pip3 install glances zfec
