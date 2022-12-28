626e9dcb051e2356b4e78d0947617f38b2f26270
mjgerm <11788121+mjgerm@users.noreply.github.com>
2022-12-28 09:44:49 -0500

    Use apt

--- install/ubuntu-kitchen-sink.sh
+++ install/ubuntu-kitchen-sink.sh
@@ -1,37 +1,37 @@
 # Updates
-apt-get update
-apt-get dist-upgrade
+apt update
+apt dist-upgrade
 
 # CLI tools
 snap install lxd
-apt-get install vim tmux
-apt-get install htop iotop iftop
-apt-get install net-tools nmap curl wget
+apt install vim tmux
+apt install htop iotop iftop
+apt install net-tools nmap curl wget
 
 # System tools
-apt-get install sshfs nwipe zstd
-#apt-get install zfsutils-linux cifs-utils
+aptt install sshfs nwipe zstd
+#apt install zfsutils-linux cifs-utils
 
 # GUI tools
-apt-get install pinta keepassxc gnuplot
+apt install pinta keepassxc gnuplot
 
 # Development (Generic)
 snap install --classic code
-apt-get install build-essential git
+apt install build-essential git
 
 # Development (C)
-#apt-get install cmake nasm gdb
-#apt-get install libc-devtools libfftw3-dev
+#apt install cmake nasm gdb
+#apt install libc-devtools libfftw3-dev
 
 # Development (Matlab)
-apt-get install octave octave-*
+apt install octave octave-*
 
 # Development (Python)
-apt-get install python3 python3-pip
+apt install python3 python3-pip
 pip3 install scipy matplotlib tensorflow tqdm serial requests glances
 #pip3 install beautifulsoup4 Pillow PyGTK wxPython zfec
 
 # Optional Addins
-# apt-get install nvidia nvidia-cuda-toolkit
+# apt install nvidia nvidia-cuda-toolkit
 # pip3 install cupy
-# apt-get install virtualbox virtualbox-ext-pack
+# apt install virtualbox virtualbox-ext-pack
